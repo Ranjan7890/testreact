8cc016025ce26d0a0cc36d706156a96b6acc77dd
Ranjan Arulanandham <10226@NORTHRISE.MAIN>
2023-07-07 16:12:02 +0200

    routing added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
-
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import Signin from "./Pages/Signin";
+import Offers from "./Pages/Offers";
+import Signup from "./Pages/Signup";
+import ForgotPassword from "./Pages/ForgotPassword";
 function App() {
   return (
     <div >
-      <h1 className=''>Ranjan</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/Sign-in" element={<Signin/>}/>
+          <Route path="/sign-up" element={<Signup/>}/>
+          <Route path="/offers" element={<Offers/>}/>
+          <Route path="/forgot-password" element={<ForgotPassword/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
