3cd8b01289f2630e08a0dcbf7444133842923124
Ranjan Arulanandham <10226@NORTHRISE.MAIN>
2023-07-11 12:31:52 +0200

    This is a header section

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import Signin from "./Pages/Signin";
 import Offers from "./Pages/Offers";
 import Signup from "./Pages/Signup";
 import ForgotPassword from "./Pages/ForgotPassword";
+import Header from "./componenets/Header";
 function App() {
   return (
     <div >
       <Router>
+       <Header/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/profile" element={<Profile/>}/>
-          <Route path="/Sign-in" element={<Signin/>}/>
+          <Route path="/sign-in" element={<Signin/>}/>
           <Route path="/sign-up" element={<Signup/>}/>
           <Route path="/offers" element={<Offers/>}/>
           <Route path="/forgot-password" element={<ForgotPassword/>}/>
